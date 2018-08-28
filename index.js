65a4665a3bb01330539d17c5525ddb1c45f65495
armandleleveur <36453540+armandleleveur@users.noreply.github.com>
2018-08-28 12:54:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -225,6 +225,52 @@ client.on(`message`, message => {
             message.channel.send(`${mute.user.username} n'est plus mute !`);
         })
     }
+    
+    if(message.content.startsWith(prefix + "softban")) {
+        let bUser   = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    let bReason = args.join(" ").slice(22);
+    if (!bUser) return message.channel.send("**>>>** Invalid User.");
+    if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("**>>>** You do not have the permision to Softban users.");
+    if(bUser.hasPermission("ADMINISTRATOR")) return message.channel.send("**>>>** You cannot Softban this user.")
+
+    let banEmbed = new Discord.RichEmbed()
+    .setTitle("~ **SoftBan** ~")
+    .setColor("#606060")
+    .addField("**~**User**~**", bUser)
+    .addField("**~**Author**~**", message.author)
+    .addField("**~**Channel**~**", message.channel)
+    .addField("**~**Time**~**", moment(message.createdAt).format(`MMMM Do YYYY, h:mm a`))
+    .addField("**~**Reason**~**", bReason);
+
+    let banAnnounce = new Discord.RichEmbed()
+    .setColor("#606060")
+    .setDescription(`User ${bUser} got SoftBanned from the server!`);
+
+    let banDm = new Discord.RichEmbed()
+    .setColor("#606060")
+    .setDescription(`**>>>** You got SoftBanned from ${message.guild.name}!\n**>>>** Reason: ${bReason}\n**>>>** You can join again immediately.`);
+
+    try {
+        message.guild.channels.get(jChannel).createInvite().then(invite =>
+            bUser.send(invite.url)
+        );
+        bUser.send(banDm);
+    } catch(e) {
+        console.error(e.stack);
+    }
+
+    let botChannel = message.guild.channels.find(`name`, "bot-reports");
+    if (!botChannel) return message.channel.send("**>>>** Could not locate Bot Report channel!\n**>>>>** Please create a channel called ``bot-reports``.");
+
+    message.delete().catch(O_o=>{});
+    message.channel.send(banAnnounce).then(msg => msg.delete().catch(O_o=>{}));
+    message.guild.member(bUser).ban(bReason);
+    message.guild.unban(bUser.id).catch(O_o=>{});
+    botChannel.send(banEmbed);
+    console.log(`User ${bUser} got SoftBanned by ${message.author}.`);
+
+}
+    }
 
 var fs = require('fs');
 
